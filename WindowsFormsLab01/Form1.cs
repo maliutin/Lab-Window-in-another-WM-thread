d4904fd34cb4565aced7163d3968b5f7630ec015
Leonid Maliutin <maliutin@sqlmaze.com>
2018-08-13 17:23:59 +0300

    add first version

---
+++ WindowsFormsLab01/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsLab01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = null;
+
+            using (var t2 = new SecondWindowsThread())
+            {
+                t2.SynchronizationContext.Send(
+                    (_) =>
+                    {
+                        form2 = new Form2();
+                        form2.Show();
+                    }, null);
+
+                Thread.Sleep(1000 * 10); // что-то долго делает.
+
+                t2.SynchronizationContext.Send((_) => form2.Close(), null);
+            }
+        }
+    }
+}
---
+++ WindowsFormsLab01/SecondWindowsThread.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsLab01
+{
+    class SecondWindowsThread : IDisposable
+    {
+        private Thread thread;
+        private SynchronizationContext ctx;
+        private ManualResetEvent mre;
+
+        public SecondWindowsThread()
+        {
+            this.mre = new ManualResetEvent(false);
+            try
+            {
+                this.thread = new Thread(
+                    () =>
+                    {
+                        Application.Idle += this.Initialize;
+                        Application.Run();
+                    }
+                );
+                thread.IsBackground = true;
+                thread.SetApartmentState(ApartmentState.STA);
+                thread.Name = "WaitDialog";
+                thread.Start();
+                this.mre.WaitOne();
+            }
+            finally
+            {
+                this.mre.Dispose();
+            }
+        }
+
+        private void Initialize(Object sender, EventArgs e)
+        {
+            this.ctx = SynchronizationContext.Current;
+            this.mre.Set();
+            Application.Idle -= this.Initialize;
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    if (this.ctx != null)
+                    {
+                        this.ctx.Send((_) => Application.ExitThread(), null);
+                        this.ctx = null;
+                    }
+                }
+                disposedValue = true;
+            }
+        }
+
+        void IDisposable.Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+
+        public SynchronizationContext SynchronizationContext
+        {
+            get { return this.ctx; }
+        }
+
+    }
+}
