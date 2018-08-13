4e647c9937e52e25e7797aacb342534baf2a6f3c
Leonid Maliutin <mals@live.ru>
2018-08-13 17:23:59 +0300

    v1.0

---
+++ src/WindowsFormsLab01/Form2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsLab01
+{
+    public partial class Form2 : Form
+    {
+        private Int32 number;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            this.timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.number += 1;
+            this.label1.Text = this.number.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.label2.Text += " Hi!";
+        }
+    }
+}
---
+++ src/WindowsFormsLab01/SecondWindowsThread.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsLab01
+{
+    class SecondWindowsThread : IDisposable
+    {
+        private SynchronizationContext ctx;
+
+        public SecondWindowsThread()
+        {
+            ManualResetEvent mre = new ManualResetEvent(false);
+            try
+            {
+                Thread thread = new Thread(
+                    () =>
+                    {
+                        this.ctx = new WindowsFormsSynchronizationContext();
+                        mre.Set();
+                        Application.Run();
+                    }
+                );
+                thread.IsBackground = true;
+                thread.SetApartmentState(ApartmentState.STA);
+                thread.Start();
+                mre.WaitOne();
+            }
+            finally
+            {
+                mre.Dispose();
+            }
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        public void Dispose()
+        {
+            if (disposedValue) return;
+
+            if (this.ctx != null)
+            {
+                this.ctx.Send((_) => Application.ExitThread(), null);
+                this.ctx = null;
+            }
+            disposedValue = true;
+        }
+
+        #endregion
+
+        public SynchronizationContext SynchronizationContext
+        {
+            get
+            {
+                if (this.disposedValue) throw new ObjectDisposedException(nameof(SynchronizationContext));
+                return this.ctx;
+            }
+        }
+
+    }
+}
